6e5d3e3be1e4600b173fdefe40cd5c4f6fb29a3c
@960bg Mihail Dementev <-h>
2022-08-08 18:42:52 +0400

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let num = Math.abs(n);
+  num = ('' + num).split('').reverse().join('');
+  // 
+  return num;
+
+
+
 }
+
+
+// console.log(reverse(-526));
